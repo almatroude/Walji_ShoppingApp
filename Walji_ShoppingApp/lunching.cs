77be7f72cc58dfd4d8854eb9c4b8d924f71e3cbe
almatroude <abdulaibawam@gmail.com>
2022-08-31 01:05:25 +0100

    lunching form created

---
+++ Walji_ShoppingApp/lunching.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Walji_ShoppingApp
+{
+    public partial class lunching : Form
+    {
+        public lunching()
+        {
+            InitializeComponent();
+        }
+        int startpoint = 0;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            startpoint += 1;
+            Myprogress.Value = startpoint;
+            if(Myprogress.Value == 100)
+            {
+                Myprogress.Value = 0;
+                timer1.Stop();
+                Form1 log = new Form1();
+                this.Hide();
+                log.Show();
+            }
+        }
+
+        private void lunching_Load(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+    }
+}
